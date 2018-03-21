a403f428fc9a9ff28d5073e7ba8d365a7e98c912
Anthony Lu <anthony.x.lu@pwc.com>
2018-03-21 17:55:45 +0800

    MYSQL

---
+++ lesson5/mysql.js
@@ -0,0 +1,63 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "work168168",
+    database: "nodedb"
+});
+
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+
+    // var sql = "ALTER TABLE customers ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY";
+    // var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
+    // var sql = "INSERT INTO customers (name, address) VALUES ?";
+    // var values = [
+    //     ['John', 'Highway 71'],
+    //     ['Peter', 'Lowstreet 4'],
+    //     ['Amy', 'Apple st 652'],
+    //     ['Hannah', 'Mountain 21'],
+    //     ['Michael', 'Valley 345'],
+    //     ['Sandy', 'Ocean blvd 2'],
+    //     ['Betty', 'Green Grass 1'],
+    //     ['Richard', 'Sky st 331'],
+    //     ['Susan', 'One way 98'],
+    //     ['Vicky', 'Yellow Garden 2'],
+    //     ['Ben', 'Park Lane 38'],
+    //     ['William', 'Central st 954'],
+    //     ['Chuck', 'Main Road 989'],
+    //     ['Viola', 'Sideway 1633']
+    // ];
+
+    // var sql = "SELECT * FROM customers WHERE address LIKE 'S%'";
+    // var adr = 'Mountain 21';
+    // var sql = 'SELECT * FROM customers WHERE address = ' + mysql.escape(adr);
+    // con.query(sql, function (err, result,fields) {
+    //     if (err) throw err;
+    //     console.log(JSON.stringify(result));
+    // });
+
+    // var adr = 'Mountain 21';
+    // var sql = 'SELECT * FROM customers WHERE address = ?';
+    // con.query(sql, [adr], function (err, result) {
+    //     if (err) throw err;
+    //     console.log(result);
+    // });    
+
+    // var name = 'Amy';
+    // var adr = 'Mountain 21';
+    // var sql = 'SELECT * FROM customers WHERE name = ? OR address = ?';
+    // con.query(sql, [name, adr], function (err, result) {
+    //     if (err) throw err;
+    //     console.log(JSON.stringify(result));
+    // });
+
+
+    con.query("SELECT * FROM customers ORDER BY name DESC", function (err, result) {
+        if (err) throw err;
+        console.log(JSON.stringify(result));
+    });
+
+});
